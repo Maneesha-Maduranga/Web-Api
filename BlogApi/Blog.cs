bb28fa31cac5cf446d625317d5307f2a5b153677
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2023-11-14 20:33:47 +0530

    Implement The Blog Model

---
+++ BlogApi/Blog.cs
@@ -0,0 +1,11 @@
+﻿namespace BlogApi
+{
+    public class Blog
+    {
+        public int Id { get; set; } 
+        public string author { get; set; } = string.Empty;
+        public string title { get; set; } = string.Empty;
+        public string content { get; set; } = String.Empty;
+
+    }
+}

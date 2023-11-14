c3f80322a5e3b084eb1c30bc19348378dbb74aaf
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2023-11-14 21:46:44 +0530

    register Database Context

--- BlogApi/Program.cs
+++ BlogApi/Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using BlogApi.Model;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<BlogContext>(options => options.UseInMemoryDatabase("Blogs"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
